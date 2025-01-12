84dc23ebc0c7509a94b60eaf4aaea97b42b628bf
JetPi <ibrahimzarragaam@gmail.com>
2025-01-12 17:54:48 -0400

    Build data (is any of this needed?)

--- MedicarApi/obj/Debug/net9.0/MedicarApi.AssemblyInfo.cs
+++ MedicarApi/obj/Debug/net9.0/MedicarApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicarApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50464e83e269180ed869420dd1b877a6f6d108f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4736b500103518ba7d6435d4b2e595f2e5695806")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicarApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicarApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
